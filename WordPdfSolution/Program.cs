f8d79dc2b561da2b62b5f891bad0cfc7d5e2d598
mostafakiswanidev@gmail.com <mostafakiswanidev@gmail.com>
2021-07-12 21:23:17 +0300

    Add project files.

---
+++ WordPdfSolution/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.IO;
+using WordToPDF;
+
+namespace WordPdfSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = "D:\\WOrdToPDF\\word.docx";
+            string name = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            string outputFile = "D:\\WOrdToPDF\\" + name + ".docx";
+
+            // Copy Word document.
+            File.Create(outputFile).Dispose();
+
+            File.Copy(inputFile, outputFile, true);
+            // Open copied document.
+            using (var flatDocument = new FlatDocument(outputFile))
+            {
+                // Search and replace document's text content.
+                flatDocument.FindAndReplace("انثى", "ذكر");
+                flatDocument.FindAndReplace("Qudamah", "Mostafa");
+            }
+
+            Word2Pdf objWorPdf = new Word2Pdf();
+            #region ToReplace
+            string backfolder1 = "D:\\WOrdToPDF\\";
+            //string strFileName = "word.docx";
+            string strFileName = name + ".docx";
+            #endregion
+
+            object FromLocation = backfolder1 + "\\" + strFileName;
+            string FileExtension = Path.GetExtension(strFileName);
+            string ChangeExtension = strFileName.Replace(FileExtension, ".pdf");
+            if (FileExtension == ".doc" || FileExtension == ".docx")
+            {
+                object ToLocation = backfolder1 + "\\" + ChangeExtension;
+                objWorPdf.InputLocation = FromLocation;
+                objWorPdf.OutputLocation = ToLocation;
+                objWorPdf.Word2PdfCOnversion();
+            }
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
